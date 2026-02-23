3f377c49a7b1fbf94a9bd28c45b60b9792f7279e
treyenwilson-peopleshores <treyen.wilson@peopleshores.com>
2026-02-23 15:58:33 +0000

    Finished GLAB 320H.7.1

--- movie-search/src/App.jsx
+++ movie-search/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import logo from "./assets/react.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Import our components
+import MovieDisplay from "./components/MovieDisplay.jsx";
+import Form from "./components/Form.jsx";
+
+export default function App() {
+  // Constant with your API Key
+  const apiKey = "c585bee0";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async(searchTerm) => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch(e) {
+      console.error(e)
+    }
+  }
+
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
 }
-
-export default App
